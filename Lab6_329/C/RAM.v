2b4b1d15df4f570a5f9e8432fea0c21b4b1a0075
Yu-Tsai <37035984+Yu-Tsai@users.noreply.github.com>
2019-03-29 10:07:16 +0800

    Add files via upload

---
+++ Lab6_329/C/RAM.v
@@ -0,0 +1,43 @@
+//*************************************************
+//** 2019 spring iVCAD
+//** Description: Random-access memory
+//** Mar. 2018 Henry authored
+//** Mar. 2019 Kevin revised
+//*************************************************
+
+`timescale 1ns/10ps
+`define ramsize 16
+`define wordsize 24
+
+module RAM (CK, A, WE, OE, D, Q);
+
+  input         CK;
+  input  [`ramsize-1:0]  A;
+  input         WE;
+  input         OE;
+  input  [`wordsize-1:0] D;
+  output [`wordsize-1:0] Q;
+
+  reg    [`wordsize-1:0] Q;
+  reg    [`ramsize-1:0]  latched_A;
+  reg    [`wordsize-1:0] memory [0:65535];
+
+  always @(posedge CK) begin
+    if (WE) begin
+      memory[A] <= D;
+    end
+    latched_A <= A;
+	
+  end
+
+  always @(*) begin
+    if (OE) begin
+      Q = memory[latched_A];
+    end
+    else begin
+      Q = 24'hz; 
+    end
+  end
+
+endmodule
+
---
+++ Lab6_329/D/MixColor.v
@@ -0,0 +1,23 @@
+//*************************************************
+//** 2019 spring iVCAD
+//** Description: Mix color of two pixels
+//** Mar. 2018 Henry authored
+//** Mar. 2019 Kevin revised
+//*************************************************
+
+`timescale 1ns/10ps
+module MixColor (color_in1, color_in2, color_out);
+
+  input  [23:0] color_in1;
+  input  [23:0] color_in2;
+  output [23:0] color_out;
+
+  //reg    [23:0] color_out;
+
+  /*always @(*) begin
+    color_out = 
+  end*/
+  
+  assign color_out = color_in1 ^ color_in2;
+
+endmodule
